6cbb318470e1dba62973cad703704ae8115fd873
dennis07 <den.goehlert@t-online.de>
2017-11-24 17:26:07 +0100

    added unit
    Signed-off-by: dennis07 <den.goehlert@t-online.de>

---
+++ backend/Smove.Data.Prolog.Utils.pas
@@ -0,0 +1,28 @@
+unit Smove.Data.Prolog.Utils;
+
+interface
+
+type
+  TProlog = class
+  public
+    constructor Create;
+    destructor Destroy; override;
+  end;
+
+implementation
+
+{ TProlog }
+
+constructor TProlog.Create;
+begin
+  inherited;
+
+end;
+
+destructor TProlog.Destroy;
+begin
+
+  inherited;
+end;
+
+end.
